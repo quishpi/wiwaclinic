a7a4124a47858b1985fcb7536eb65c3f75c061cb
luisquishpi <luis.quishpi@gmail.com>
2019-11-01 17:01:11 +0100

    Updated especies Bean

--- src/main/java/ec/wiwaclinic/vista/bean/especie/EspecieListaBean.java
+++ src/main/java/ec/wiwaclinic/vista/bean/especie/EspecieListaBean.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import ec.wiwaclinic.controlador.especie.EspecieControlador;
+import ec.wiwaclinic.controlador.EspecieControlador;
 import ec.wiwaclinic.modelo.dao.EspecieDao;
 import ec.wiwaclinic.modelo.entidad.Especie;
 
