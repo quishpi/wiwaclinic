b89d58fb5ef476baec57aa2951be2b034b9f8200
luisquishpi <luis.quishpi@gmail.com>
2019-11-07 16:45:36 +0100

    Added: Validacion de cedula

--- src/main/java/ec/wiwaclinic/utils/Utils.java
+++ src/main/java/ec/wiwaclinic/utils/Utils.java
@@ -27,4 +27,73 @@ public class Utils {
 		return picInBytes;
 	}
 
+	public static Boolean validarRuc(String numced) {
+		if (numced.length() != 13) {
+			return false;
+		}
+		String cedula = numced.substring(0, 10);
+		String cerocerouno = numced.substring(10, 13);
+		if (!cerocerouno.equals("001")) {
+			return false;
+		}
+
+		return validarCedula(cedula);
+	}
+
+	public static Boolean validarCedula(String numced) {
+
+		if (numced.isEmpty()) {
+			return false;
+		}
+		Integer tam = numced.length();// ontiene el numero de digitos de numced
+		Integer i = 0;// contador
+		Integer suma = 0;// almacena la suma de la multiplicacion de los 8 digitos primeros
+		Integer ban = 2;// para multiplicar con 2 o 1
+		Integer comp;// calcula el valor resultante restando del inmediato superior para comparar
+		if (tam == 10) {
+			for (i = 0; i < 9; i++) { // multiplica por 2 o 1 los nueve primeros digitos
+				// almacena la suma de los resultados en sum.
+				int mul = 0;
+				int aux = 0;// variables auxiliar temporal
+				aux = Integer.parseInt(Character.toString(numced.charAt(i)));
+				if (ban == 2) {
+					mul = aux * 2;
+					ban = 1;
+				} else {
+					if (ban == 1) {
+						mul = aux * 1;
+						ban = 2;
+					}
+				}
+				if (mul > 9) {
+					mul = mul - 9;
+				}
+				suma = suma + mul;
+			}
+
+			if (suma.toString().substring(1, 1).equals("0")) {
+				comp = 0;
+			} else { // resta la suma de los 9 digitos del mediato superior para
+				// comparar con el ultimo digito de la cedula
+				comp = Integer.parseInt(Character.toString(suma.toString().charAt(0))) + 1;
+				comp = comp * 10;
+				comp = comp - suma;
+			}
+			// Compara con el ultimo digito
+			if (Character.toString(numced.charAt(9)).equals(comp.toString())) {
+				return true;// CEDULA CORRECTO
+			} else {
+				return false;
+			} // CEDULA INCORRECTO
+		} else {
+			if (tam < 10) {
+				return false;// < 10 caracteres
+			}
+			if (tam > 10) {
+				return false;// > 10 caracteres
+			}
+		} // fin if (tam==10)
+		return true;
+	}
+
 }
